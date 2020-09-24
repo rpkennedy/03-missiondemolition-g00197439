87decbf3df9a0af2769933b93982b0045046ddff
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-09-24 08:43:47 -0400

    MissionDemolition v1.1
    Clouds fixed
    Another castle added

--- Assets/__Scripts/CloudController.cs
+++ Assets/__Scripts/CloudController.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class CloudController : MonoBehaviour
 {
     [Header("Set in Unity")]
-    public int numClouds = 40;      
-    public GameObject cloudPrefab;
-    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
-    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
-    public float cloudScaleMin = 1;
-    public float cloudScaleMax = 3;
-    public float cloudSpeedMult = 0.5f;
-    private GameObject[] cloudInstances;
+    public GameObject cloudSphere;
+    public int cloudMin;
+    public int cloudMax;
+
 
     private Vector3 scaleOffset = new Vector3(5, 2, 1);
     private Vector2 scaleRangex = new Vector2(4, 8);
@@ -21,38 +17,14 @@ public class CloudController : MonoBehaviour
     private float scaleMiny = 2f;
     private List<GameObject> spheres;
 
-
-    void Awake()
-    {
-        cloudInstances = new GameObject[numClouds];
-        GameObject anchor = GameObject.Find("CloudAnchor");
-        GameObject cloud;
-
-        for (int i = 0; i < numClouds; i++)
-        {
-            cloud = Instantiate<GameObject>(cloudPrefab);
-            Vector3 cPos = Vector3.zero;
-            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
-            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
-            float scaleU = Random.value;
-            float scaleVal = Mathf.Lerp(cloudScaleMin, cloudScaleMax, scaleU);
-            cPos.y = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
-            cPos.z = 100 - 90 * scaleU;
-            cloud.transform.position = cPos;
-            cloud.transform.localScale = Vector3.one * scaleVal;
-            cloud.transform.SetParent(anchor.transform);
-            cloudInstances[i] = cloud;
-        }
-    }
-
     void Start()
     {
         spheres = new List<GameObject>();
-        float num = Random.Range(cloudScaleMin, cloudScaleMax);
+        int num = Random.Range(cloudMin, cloudMax);
 
         for (int i = 0; i < num; i++)
         {
-            GameObject sp = Instantiate<GameObject>(cloudPrefab);
+            GameObject sp = Instantiate<GameObject>(cloudSphere);
             spheres.Add(sp);
             Transform spTrans = sp.transform;
             spTrans.SetParent(this.transform);
@@ -76,18 +48,7 @@ public class CloudController : MonoBehaviour
 
     void Update()
     {
-        foreach (GameObject cloud in cloudInstances)
-        {
-            float scaleVal = cloud.transform.localScale.x;
-            Vector3 cPos = cloud.transform.position;
-            cPos.x -= scaleVal * Time.deltaTime * cloudSpeedMult;
-            
-            if (cPos.x <= cloudPosMin.x)
-            {
-                cPos.x = cloudPosMax.x;
-            }
-            cloud.transform.position = cPos;
-        }
+        
     }
 
     void Restart()
