d6aa5b808c6a8076d5baf9abc6fe5cffb869773c
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-09-23 21:10:02 -0400

    MissionDemolition v0.2
    Projectile prefabbed
    Slingshot script in progress
    Halo added

---
+++ Assets/Scripts/SlingshotController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingshotController : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+
+    void Awake()
+    {
+        Transform launchTrans = transform.Find("LaunchPoint");
+        launchPoint = launchTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+}
