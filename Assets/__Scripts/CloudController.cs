978ded391e03b86c3731b09f316fd79d7e1657d4
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-09-24 00:03:16 -0400

    MissionDemolition v0.5
    Camera script
    Castle making
    Clouds (alpha)

---
+++ Assets/__Scripts/CloudController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    [Header("Set in Unity")]
+    public GameObject cloudSphere;
+    public int cloudMin;
+    public int cloudMax;
+
+
+    private Vector3 scaleOffset = new Vector3(5, 2, 1);
+    private Vector2 scaleRangex = new Vector2(4, 8);
+    private Vector2 scaleRangey = new Vector2(3, 4);
+    private Vector2 scaleRangez = new Vector2(2, 4);
+    private float scaleMiny = 2f;
+    private List<GameObject> spheres;
+
+    void Start()
+    {
+        spheres = new List<GameObject>();
+        int num = Random.Range(cloudMin, cloudMax);
+
+        for (int i = 0; i < num; i++)
+        {
+            GameObject sp = Instantiate<GameObject>(cloudSphere);
+            spheres.Add(sp);
+            Transform spTrans = sp.transform;
+            spTrans.SetParent(this.transform);
+
+            Vector3 offset = Random.insideUnitSphere;
+            offset.x *= scaleOffset.x;
+            offset.y *= scaleOffset.y;
+            offset.z *= scaleOffset.z;
+            spTrans.localPosition = offset;
+
+            Vector3 scale = Vector3.one;
+            scale.x = Random.Range(scaleRangex.x, scaleRangex.y);
+            scale.y = Random.Range(scaleRangey.x, scaleRangey.y);
+            scale.z = Random.Range(scaleRangez.x, scaleRangez.y);
+
+            scale.y *= 1 - (Mathf.Abs(offset.x) / scaleOffset.x);
+            scale.y = Mathf.Max(scale.y, scaleMiny);
+            spTrans.localScale = scale;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Restart();
+        }
+    }
+
+    void Restart()
+    {
+        foreach (GameObject sp in spheres)
+        {
+            Destroy(sp);
+        }
+
+        Start();
+    }
+}
---
+++ Assets/__Scripts/RigidbodySleep.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodySleep : MonoBehaviour
+{
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if (rb != null) rb.Sleep();
+    }
+
+    void Update()
+    {
+        
+    }
+}
