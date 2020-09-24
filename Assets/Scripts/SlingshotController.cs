0ef9ef39ed9c2647a09a4ddc809385e6f1e2f2fe
rpkennedy <55095545+rpkennedy@users.noreply.github.com>
2020-09-23 21:40:17 -0400

    MissionDemolition v0.3
    Slingshot scripting
    Projectile spawns and launches

--- Assets/Scripts/SlingshotController.cs
+++ Assets/Scripts/SlingshotController.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class SlingshotController : MonoBehaviour
 {
+    [Header("Set in Unity")]
+    public GameObject prefabProjectile;
+    public float velocityMult;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimMode;
+    private Rigidbody rbProjectile;
 
 
     void Awake()
@@ -12,11 +21,36 @@ public class SlingshotController : MonoBehaviour
         Transform launchTrans = transform.Find("LaunchPoint");
         launchPoint = launchTrans.gameObject;
         launchPoint.SetActive(false);
+        launchPos = launchTrans.position;
     }
 
     void Update()
     {
-        
+        if (!aimMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimMode = false;
+            rbProjectile.isKinematic = false;
+            rbProjectile.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
     }
 
     void OnMouseEnter()
@@ -24,8 +58,19 @@ public class SlingshotController : MonoBehaviour
         launchPoint.SetActive(true);
     }
 
-    private void OnMouseExit()
+    void OnMouseExit()
     {
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimMode = true;
+
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+
+        rbProjectile = projectile.GetComponent<Rigidbody>();
+        rbProjectile.isKinematic = true;
     }
 }
